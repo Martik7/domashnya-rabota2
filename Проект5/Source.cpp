809448d01c28168298300ee57fdcee72d0fe6231
Олжас <@->
2018-11-25 16:50:09 +0600

    Добавьте файлы проекта.

---
+++ Проект5/Source.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <iostream>
+#include <locale.h>
+
+
+int main()
+{
+
+
+	/*float TF, TC;
+
+	TF = 256;
+	TC = (TF - 32) * 5 / 9;
+	printf("%f\n", TC);*/
+
+
+	/*int a, c;
+
+	a = 2;
+
+	c = pow(a, 15);
+	printf("%d\n", c);*/
+
+
+	/*int y, x;
+
+	x = 5;
+	y = 4 * (pow(x - 3, 6)) - 7 * (pow(x - 3, 3)) + 2;
+	printf("%d\n", y);*/
+
+
+	/*int a,c;
+	
+	a = 2;
+
+	c = pow(a, 8);
+	printf("%d\n", c);*/
+
+
+
+	/*int y, x;
+	
+	x = 6;
+	y = 3 * pow(x, 6) - 6 * pow(x, 2) - 7;
+	printf("%d\n", y);*/
+
+	system("pause");
+
+}
